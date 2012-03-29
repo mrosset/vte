a57ba587aedfa4d3c37476dba14c83ac6da2d049
str1ngs <mike.rosset@gmail.com>
2012-03-29 02:42:22 -0700

    use go get friendly imports

--- goterm/main.go
+++ goterm/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"github.com/mattn/go-gtk/gtk"
+	"github.com/str1ngs/vte"
 	"os"
-	"vte"
 )
 
 func main() {
 	gtk.Init(&os.Args)
 	window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL)
-	terminal := NewTerminal()
+	terminal := vte.NewTerminal()
 	swin := gtk.ScrolledWindow(nil, nil)
 	swin.SetPolicy(gtk.GTK_POLICY_NEVER, gtk.GTK_POLICY_NEVER)
 	terminal.Fork("bash")
